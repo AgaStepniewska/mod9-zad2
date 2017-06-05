63ac53bc113ac9977644db4a50dd2d116fdf7417
pinko <agaslota@wp.pl>
2017-06-05 13:02:22 +0100

    usunieto console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 var maleNames = ['Tomek', 'Jarek', 'Stasiu', 'Marek', 'Arek'];
 var femaleNames = ['Agnieszka', 'Asia', 'Kasia', 'Ewa', 'Ula'];
 var allNames = maleNames.concat(femaleNames);
-
 console.log(allNames);
 
 var newName = 'Krzysiu';
 if(allNames.indexOf(newName) === -1) {
-	console.log(allNames.push(newName)); 
+	allNames.push(newName); 
 }
 console.log(allNames);
